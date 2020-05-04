a6b51a34c5e1bebed842f4aa1a5fe3cb394bfcd8
Mariana Pereira <marianna.22sp@gmail.com>
2020-05-04 16:35:54 -0300

    User's CRUD

---
+++ src/routes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express'
+import UserController from './controllers/UserController'
+
+const routes = Router()
+
+routes.get('/users', UserController.index)
+routes.post('/users', UserController.store)
+routes.put('/users/:id', UserController.update)
+routes.delete('/users/:id', UserController.delete)
+
+export default routes
